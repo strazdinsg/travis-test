854231740b349cb9855166c0edfb9510c57b1147
Girts <girts.strazdins@gmail.com>
2019-03-23 14:55:36 +0100

    Add plain Spring boot project, without Travis setup

---
+++ src/main/java/ntnu/no/travisspringdemo/TravisSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package ntnu.no.travisspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisSpringDemoApplication.class, args);
+	}
+
+}
